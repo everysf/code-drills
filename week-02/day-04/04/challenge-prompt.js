6380be2f6af145fcc4a012fc06fb3f6c7a21967e
Kevin M <kevin@everysf.com>
2018-08-05 21:59:27 -0700

    push, still finishing day 5

---
+++ week-02/day-04/04/challenge-prompt.js
@@ -0,0 +1,31 @@
+// Section 1 
+// Set a variable of userTyped and use get element by id or query selector and set it equal to the element with the id of letterTyped 
+
+var userTyped = $("#letterTyped");
+
+// Set a variable of letterUsed and use get element by id or query selector and set it equal to the element with the id of lastLetter 
+
+var letterUsed = $("#lastLetter")
+
+// create a variable called LetterUsedArray and have it equal and empty array
+
+var letterUsedArray = [];
+
+// Section 2
+// now use the onkey up jquery function and pass it the arguement of event
+
+
+$(event).keyup(
+    var userInput = event.key
+     
+
+)
+
+    // now set a variable of userInput equal to the event.key
+
+    // use the userTyped variable and the textContent property equal the userInput
+
+    // push the userInput into the letterUsedArray
+
+    // use the letterUSed variable and the textContent property plus equal the userInput plus a string ,
+
